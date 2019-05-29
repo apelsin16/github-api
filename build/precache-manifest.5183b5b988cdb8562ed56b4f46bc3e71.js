58eac196656e1d007dfbd1254e33fa1c337eb31d
Apelsin16 <apelsin@i.ua>
2019-05-29 20:01:39 +0300

    ready

---
+++ build/precache-manifest.5183b5b988cdb8562ed56b4f46bc3e71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77a6e322945c30fc905c29da7e661c8",
+    "url": "/github-api/build/index.html"
+  },
+  {
+    "revision": "c79898e6b5c50bf9b19e",
+    "url": "/github-api/build/static/css/main.628417d5.chunk.css"
+  },
+  {
+    "revision": "2d3e8ef2a432821ee68b",
+    "url": "/github-api/build/static/js/2.8c1e8193.chunk.js"
+  },
+  {
+    "revision": "c79898e6b5c50bf9b19e",
+    "url": "/github-api/build/static/js/main.eeccf61f.chunk.js"
+  },
+  {
+    "revision": "b4801400c97b260cbde9",
+    "url": "/github-api/build/static/js/runtime~main.4f639871.js"
+  }
+]);
